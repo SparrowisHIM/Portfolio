909255fcb0b59728e9e50a1b15a2438ad4e5abea
Efe Ebomwonyi <ebomwonyiefe@gmail.com>
2026-09-17 18:13:29 +0100

    style: the setting-out grid drops back to a whisper on black

--- src/components/scene/Ground.tsx
+++ src/components/scene/Ground.tsx
@@ -4,13 +4,13 @@
 export function Ground() {
   return (
     <group>
-      <gridHelper args={[160, 64, "#1a2538", "#101827"]} position={[0, 0, 0]}>
-        <lineBasicMaterial attach="material" color="#1c2a42" transparent opacity={0.4} depthWrite={false} />
+      <gridHelper args={[96, 32, "#131c2b", "#0c131f"]} position={[0, 0, 0]}>
+        <lineBasicMaterial attach="material" color="#142032" transparent opacity={0.16} depthWrite={false} />
       </gridHelper>
       {[12.5, 12.9].map((r, i) => (
         <mesh key={r} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.02, 0]}>
           <ringGeometry args={[r, r + (i === 0 ? 0.06 : 0.02), 128]} />
-          <meshBasicMaterial color="#6f7f9f" transparent opacity={i === 0 ? 0.22 : 0.1} depthWrite={false} />
+          <meshBasicMaterial color="#5d6c88" transparent opacity={i === 0 ? 0.14 : 0.06} depthWrite={false} />
         </mesh>
       ))}
     </group>
